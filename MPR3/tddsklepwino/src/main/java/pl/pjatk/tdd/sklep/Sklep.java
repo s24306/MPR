7f0990232ad748eb5a4e8e3b50e9ce543bbcdc5d
s24306 <s24306@pjwstk.edu.pl>
2022-11-27 17:22:38 +0100

    finish lab 3

--- MPR3/tddsklepwino/src/main/java/pl/pjatk/tdd/sklep/Sklep.java
+++ MPR3/tddsklepwino/src/main/java/pl/pjatk/tdd/sklep/Sklep.java
@@ -3,6 +3,16 @@ package pl.pjatk.tdd.sklep;
 public class Sklep {
     public static void dokonajZakupu(KartaKlienta karta, double kwotaZakupu) {
         //TODO: dodaj algorytm na liczenie ilości kuponów jakie należą się za zakup
+        double upust = karta.pobierzIloscDostepnychKuponow() >= 10 ? 0.03 : 0;
+        double znizka = 0;
+        if(kwotaZakupu > 50){
+            if(kwotaZakupu > 100){
+                znizka = 0.1;
+            } else {
+                znizka = 0.05;
+            }
+        }
+        kwotaZakupu -= kwotaZakupu * ((upust + znizka)/100);
         double kupony = kwotaZakupu/40;
         if (kupony >= 3){
             karta.dodajKupony(3);
